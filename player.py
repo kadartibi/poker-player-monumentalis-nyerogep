a804f398416755b35094ad47ca83456bca46dcb9
Anna Czinege <anna.czinege3@gmail.com>
2019-12-09 10:49:20 +0100

    Bet if we have pair in hand

--- player.py
+++ player.py
@@ -3,6 +3,12 @@ class Player:
     VERSION = "0.2"
 
     def betRequest(self, game_state):
+        for element in game_state:
+            if element == "players":
+                for player in element:
+                    if player["id"] == game_state["in_action"]:
+                        if player["hole_cards"][0]["rank"] == player["hole_cards"][1]["rank"]:
+                            return game_state["current_buy_in"] - player["bet"] + game_state["minimum_raise"]
         return 0
 
     def showdown(self, game_state):
