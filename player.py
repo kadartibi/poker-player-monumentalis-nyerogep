9d00873a30364395b19e54536dab035a704d71e4
LaszloNagy85 <n.laszlo.uk@gmail.com>
2019-12-09 16:23:15 +0100

    test 248

--- player.py
+++ player.py
@@ -26,33 +26,33 @@ class Player:
         total_card_value = cards_values_dict[first_card["rank"]] + cards_values_dict[
             second_card["rank"]] + card_difference + color_value_modifier
 
-        if len(community_cards) == 0 or len(community_cards) == 4 or len(community_cards) == 5:
-            if first_card["rank"] == second_card["rank"]:
-                pair_in_hand = True
-                return game_state["current_buy_in"] - our_player["bet"]
+        # if len(community_cards) == 0 or len(community_cards) == 4 or len(community_cards) == 5:
+        if first_card["rank"] == second_card["rank"]:
+            pair_in_hand = True
+            return game_state["current_buy_in"] - our_player["bet"]
 
-            if total_card_value >= 30:
-                return game_state["current_buy_in"] - our_player["bet"]
+        if total_card_value >= 30:
+            return game_state["current_buy_in"] - our_player["bet"]
 
         # Check after flop
-        if len(community_cards) == 3:
-            highest_card_value_on_table = 0
-            color_counter[first_card["rank"]] += 1
-            color_counter[second_card["rank"]] += 1
-            for card in community_cards:
-                if cards_values_dict[card["rank"]] > highest_card_value_on_table:
-                    highest_card_value_on_table = cards_values_dict[card["rank"]]
-                color_counter[card["rank"]] += 1
-                if first_card["rank"] == card["rank"] or second_card["rank"] == card["rank"] or pair_in_hand:
-                    return game_state["current_buy_in"] - our_player["bet"]
-
-            if cards_values_dict[first_card["rank"]] > highest_card_value_on_table or \
-                    cards_values_dict[second_card["rank"]] > highest_card_value_on_table:
-                return game_state["current_buy_in"] - our_player["bet"]
-
-            for color_number in color_counter.values():
-                if color_number >= 4:
-                    return game_state["current_buy_in"] - our_player["bet"]
+        # if len(community_cards) == 3:
+        #     highest_card_value_on_table = 0
+        #     color_counter[first_card["rank"]] += 1
+        #     color_counter[second_card["rank"]] += 1
+        #     for card in community_cards:
+        #         if cards_values_dict[card["rank"]] > highest_card_value_on_table:
+        #             highest_card_value_on_table = cards_values_dict[card["rank"]]
+        #         color_counter[card["rank"]] += 1
+        #         if first_card["rank"] == card["rank"] or second_card["rank"] == card["rank"] or pair_in_hand:
+        #             return game_state["current_buy_in"] - our_player["bet"]
+        #
+        #     if cards_values_dict[first_card["rank"]] > highest_card_value_on_table or \
+        #             cards_values_dict[second_card["rank"]] > highest_card_value_on_table:
+        #         return game_state["current_buy_in"] - our_player["bet"]
+        #
+        #     for color_number in color_counter.values():
+        #         if color_number >= 4:
+        #             return game_state["current_buy_in"] - our_player["bet"]
 
         return 0
 
