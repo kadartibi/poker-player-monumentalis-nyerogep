d0648847e51a7d21fec99104b6f61927069cdda6
Anna Czinege <anna.czinege3@gmail.com>
2019-12-09 10:57:03 +0100

    Bet if we have pair in hand again and again

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player:
         our_player = game_state["players"]["our_id"]
         bet = game_state["current_buy_in"]
         if our_player["hole_cards"][0]["rank"] == our_player["hole_cards"][1]["rank"]:
-            return game_state["minimum_raise"]
+            return bet - our_player["bet"] + game_state["minimum_raise"]
         return 0
 
     def showdown(self, game_state):
