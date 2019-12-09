c81ac196f4fb11e8e5fd2cbcc0144ac1c44f9ccf
Armin Kovacs <armin0191@gmail.com>
2019-12-09 11:37:09 +0100

    Hidden commit

--- player.py
+++ player.py
@@ -3,7 +3,14 @@ class Player:
     VERSION = "0.2"
 
     def betRequest(self, game_state):
-        print("Test")
+        players = game_state["players"]
+        our_id = game_state["in_action"]
+        our_player = players[our_id]
+        first_card = our_player["hole_cards"][0]
+        second_card = our_player["hole_cards"][1]
+        if first_card["rank"] == second_card["rank"]:
+            return game_state["current_buy_in"] - our_player["bet"]
+
         return 0
 
     def showdown(self, game_state):
